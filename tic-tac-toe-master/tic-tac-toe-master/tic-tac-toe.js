6de814362650b4f6d40e08d3816a2335b74fe62b
Ojiugo Okoroafor <amaokoroafor@hotmail.com>
2021-10-10 23:24:23 -0500

    Updated Game Board

---
+++ tic-tac-toe-master/tic-tac-toe-master/tic-tac-toe.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() { 
+    let boxes=document.getElementById('board').children
+    for (var x = 0; x<=8; x++){
+        boxes[x].classList.add('square')
+        
+    }   
+});
